51343f0c28f0212155f6474cf4f8efb417bfb9dd
Federico Lanzani <federico.finder@gmail.com>
2021-06-28 17:38:10 +0200

    added create and delete board

--- project/urls.py
+++ project/urls.py
@@ -7,5 +7,6 @@ app_name = "project"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:team_name>", views.team_details, name="team_details"),
+
     path("create_team/", views.create_team, name="create_team")
 ]
--- project/views.py
+++ project/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Team, TeamMember
+from .models import Team, TeamMember, Board
 from .forms import CreateTeam
 
 
@@ -14,14 +14,38 @@ def index(request):
 
 def team_details(request, team_name):
     # Qui è la pagina del team, vengono mostrati membri e boards
-    # try:
-    #     team = Team.objects.get(pk=team_name)
-    #
-    #     return render(request, "project/team_details.html", {"team": team})
-    # except Team.DoesNotExist:
-    #     raise Http404()
+
     team = get_object_or_404(Team, pk=team_name)
-    return render(request, "project/team_details.html", {"team": team})
+
+    try:
+        boards = Board.objects.filter(team_name=team)
+    except Board.DoesNotExist:
+        pass
+
+    if request.method == "POST":
+        if request.POST.get("new_board"):
+            board_name = request.POST.get("board_name")
+            board_description = request.POST.get("board_description")
+
+            board_already_exist = (
+                    len(Board.objects.filter(name=board_name, team_name=team)) > 0
+            )
+
+            if board_already_exist:
+                print(f"[INVALID] La board {board_name} esiste già")
+            elif len(board_name) <= 45 and len(board_description) <= 255:
+                b = Board(name=board_name, description=board_description, team_name=team)
+                b.save()
+            else:
+                print("[INVALID] Il nome può essere lungo max 45 caratteri e la descrizione max 255 caratteri")
+
+        elif request.POST.get("delete_board"):
+            board_name = request.POST.get("delete_board")
+            Board.objects.filter(name=board_name, team_name=team).delete()
+
+        return HttpResponseRedirect("/project/%s" % team.name)
+
+    return render(request, "project/team_details.html", {"team": team, "boards": boards})
 
 
 def create_team(request):
