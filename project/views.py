bc0027ff23b8a40e1186484a9ac4a0485691a513
Federico Lanzani <federico.finder@gmail.com>
2021-06-28 23:07:39 +0200

    Aggiunta vista board con colonne e tiles. Aggiunta pagina profilo utente

--- project/views.py
+++ project/views.py
@@ -1,14 +1,12 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Team, TeamMember, Board
+from .models import Team, TeamMember, Board, Column, Tile
 from .forms import CreateTeam
 
 
 # Create your views here.
 
 def index(request):
-    # TODO fare elimina team come elimina board
-
     if request.user.is_authenticated:
         user_teams = TeamMember.objects.filter(user_username=request.user)
         return render(request, "project/index.html", {"user_teams": user_teams})
@@ -41,9 +39,10 @@ def team_details(request, team_name):
     if not TeamMember.objects.filter(team_name=team, user_username=request.user).exists():
         return HttpResponseRedirect("/project")
 
+    user_admin = TeamMember.objects.get(team_name=team, user_username=request.user)
+
     if request.method == "POST":
         if request.POST.get("new_board"):
-            # TODO solo un admin può creare una board
             board_name = request.POST.get("board_name")
             board_description = request.POST.get("board_description")
 
@@ -60,47 +59,80 @@ def team_details(request, team_name):
                 print("[INVALID] Il nome può essere lungo max 45 caratteri e la descrizione max 255 caratteri")
 
         elif request.POST.get("delete_board"):
-            # TODO solo un admin può eliminare una board
             board_name = request.POST.get("delete_board")
             Board.objects.filter(name=board_name, team_name=team).delete()
 
+        elif request.POST.get("delete_team"):
+            Team.objects.filter(name=team.name).delete()
+
         return HttpResponseRedirect("/project/%s" % team.name)
 
-    return render(request, "project/team_details.html", {"team": team, "boards": boards, "members": members})
+    return render(request, "project/team_details.html",
+                  {"team": team, "boards": boards, "members": members, "user_admin": user_admin})
 
 
 def create_team(request):
-    # TODO se l'utente non è autenticato non può creare il team
-    if request.method == "POST":
-        form = CreateTeam(request.POST)
-        if not form.is_valid():
-            return render(request, "project/create_team.html", {"form": form})
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = CreateTeam(request.POST)
+            if not form.is_valid():
+                return render(request, "project/create_team.html", {"form": form})
 
-        team_name = form.cleaned_data["name"]
-        team_description = form.cleaned_data["description"]
+            team_name = form.cleaned_data["name"]
+            team_description = form.cleaned_data["description"]
 
-        team_already_exist = (
-                len(Team.objects.filter(name=team_name)) > 0
-        )
-        if not team_already_exist:
-            # Aggiunta Team
-            team = Team(name=team_name, description=team_description)
-            team.save()
+            team_already_exist = (
+                    len(Team.objects.filter(name=team_name)) > 0
+            )
+            if not team_already_exist:
+                # Aggiunta Team
+                team = Team(name=team_name, description=team_description)
+                team.save()
 
-            # Aggiunta Team Member
-            team_admin = request.user
-            team_member = TeamMember(team_name=team, user_username=team_admin, role="a")
-            team_member.save()
+                # Aggiunta Team Member
+                team_admin = request.user
+                team_member = TeamMember(team_name=team, user_username=team_admin, role="a")
+                team_member.save()
 
-        return HttpResponseRedirect("/project/%s" % team_name)
+            return HttpResponseRedirect("/project/%s" % team_name)
+        else:
+            form = CreateTeam()
+        return render(request, "project/create_team.html", {"form": form})
     else:
-        form = CreateTeam()
-    return render(request, "project/create_team.html", {"form": form})
+        return HttpResponseRedirect("/login")
 
 
 def board_details(request, team_name, board_name):
-    return render(request, "project/board_details.html", {})
+    try:
+        team = Team.objects.get(pk=team_name)
+    except Team.DoesNotExist:
+        return HttpResponseRedirect("/project")
+    try:
+        board = Board.objects.get(team_name=team, name=board_name)
+    except Board.DoesNotExist:
+        return HttpResponseRedirect("/project/%s" % team_name)
+
+    try:
+        members = TeamMember.objects.filter(team_name=team)
+    except TeamMember.DoesNotExist:
+        pass
+
+    # Se l'utente corrente non è nel team selezionato reindirizza a pagina dei suoi team
+    if not TeamMember.objects.filter(team_name=team, user_username=request.user).exists():
+        return HttpResponseRedirect("/project")
+
+    try:
+        columns = Column.objects.filter(team_name=team, board_name=board)
+    except Column.DoesNotExist:
+        pass
+
+    try:
+        tiles = Tile.objects.filter(team_name=team, board_name=board)
+    except Tile.DoesNotExist:
+        pass
+
+    return render(request, "project/board_details.html", {"columns": columns, "tiles": tiles})
 
 
 def view_profile(request):
-    return None
+    return render(request, "project/view_profile.html", {})
